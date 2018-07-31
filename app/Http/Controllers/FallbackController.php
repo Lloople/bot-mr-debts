9ba49a0046701a70ee81a3fe68c51df274471bb9
David Llop <d.lloople@gmail.com>
2018-07-31 21:05:54 +0200

    Remove events from FallbackController

--- app/Http/Controllers/FallbackController.php
+++ app/Http/Controllers/FallbackController.php
@@ -10,19 +10,6 @@ class FallbackController extends Controller
 
     public function index(BotMan $bot)
     {
-        if ($this->isCreatingAGroupOrJoiningAGroup($bot)) {
-            $bot->startConversation(new RegisterGroupConversation);
-
-            return;
-        }
-
-        dump($bot->getMessage()->getPayload());
         $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
-    }
-
-    private function isCreatingAGroupOrJoiningAGroup(BotMan $bot)
-    {
-        return $bot->getMessage()->getPayload()->get('group_chat_created')
-            || ($bot->getMessage()->getPayload()->get('new_chat_members'));
     }
 }
