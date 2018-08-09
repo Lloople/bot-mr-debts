f24652ffe0ae4ee0215814ae09913c959f9cb29e
David Llop <d.lloople@gmail.com>
2018-08-09 15:17:25 +0200

    Rename Debts Test to English, create a test class for Catalan language

--- tests/Feature/CreateDebtsInEnglishTest.php
+++ tests/Feature/CreateDebtsInEnglishTest.php
@@ -8,7 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class CreateDebtsTest extends TestCase
+class CreateDebtsInEnglishTest extends TestCase
 {
 
     use RefreshDatabase;
