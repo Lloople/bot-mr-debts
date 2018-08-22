23a1d1a90b84d99d52ddcdf4cf23a496e5e0e031
David Llop <d.lloople@gmail.com>
2018-08-22 23:32:24 +0200

    Don't report custom exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,7 +13,8 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        MissingGroupException::class,
+        PrivateConversationNotAllowedException::class,
     ];
 
     /**
